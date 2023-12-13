680c7e60efb9e7ab414e91fd9f71a5cbbf886f5e
shefat26 <shefatmoral@gmail.com>
2023-12-12 19:59:32 -0500

    second commit from this file

---
+++ variable.js
@@ -0,0 +1,83 @@
+
+// Data type and Variables
+
+/*
+Undefined, null, boolean, string, symbol, number, object
+
+Undefined ==> is something that has not been defined
+
+null ==> is nothing, when we set some value to be nothing.
+
+boolean  ==> is True  or False
+
+string   ==> string we declare inside the single quotation mark ' '
+
+number  ==> is   number like  9,  1, 4   etc
+
+object ==> object can store lot of different key value data
+
+*/
+
+
+
+var myName = "james";
+
+ //console.log(myName)
+
+myName = "watson"
+console.log(myName)
+
+
+let newName = "rony"
+//console.log(newName)
+
+newName = "hyder"
+console.log(newName)
+
+
+const aNum = 25
+console.log(aNum)
+
+//aName = "Shen"
+
+
+
+
+// Storing Value with the Assignment Operator
+
+
+var a = 7;
+var b = 2;
+
+console.log( a);
+
+a = b              // here we assigne variable  "b" value into "a"
+console.log(a)
+
+
+
+
+
+
+// Initializing Variables with Assignment Operator
+
+
+var a = 7;
+console.log(a);
+
+
+// Un-initialized variables
+
+ var a;
+ var b;
+ var c;
+
+// Initializing Variables
+
+ var a = 9;
+ var b = 8;
+ var c = "A string";
+
+ console.log(a)
+ console.log(b)
+ console.log(c)
