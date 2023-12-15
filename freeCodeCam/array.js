54e6806f5c039c5838faf3f618ac4371bc7b27d1
shefat26 <shefatmoral@gmail.com>
2023-12-14 20:17:28 -0500

    array added

--- freeCodeCam/array.js
+++ freeCodeCam/array.js
@@ -4,11 +4,11 @@
 /*
                                       Topic
 
- 1)
+ 1) Store Multiple Values with Arrays
 
- 2)
+ 2) Nested Array / Multidimentional array
 
- 3)
+ 3) Access array data with index
 
  4)
 
@@ -25,6 +25,73 @@
 
 
 */
+
+
+
+
+
+// 1) Store Multiple Values with Arrays
+
+
+ var ourArray = ["John", 23];
+
+console.log(ourArray);
+
+
+
+
+
+// 2) Nested Array / Multidimentional array
+
+
+var ourArray = [["the universe", 42, 55], "ALACE",235, ["everything", 1001]]
+
+console.log(ourArray[0][2])
+console.log(ourArray[1][3])
+console.log(ourArray[3][0])
+console.log(ourArray[3][1])
+console.log(ourArray[2])
+
+
+
+
+
+
+
+
+// 3) Access array data with index
+
+
+var anArray = [50, 60, 70, 8]
+
+console.log(anArray[1]);
+console.log(anArray[0]);
+console.log(anArray[3]);
+
+
+
+
+
+
+// Modify Array Data with Index
+
+
+var myArray = [20,64,99,33]
+console.log(myArray);
+
+myArray[0] = 85;
+console.log(myArray)
+
+
+console.log("\n", "\n###########################\n")
+
+
+
+
+
+
+
+
 
 
 
