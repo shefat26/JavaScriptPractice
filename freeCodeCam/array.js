e75d4f1d6a965dca1f96c25999ecdd21297ed256
shefat26 <shefatmoral@gmail.com>
2023-12-15 19:57:00 -0500

    Array practice

--- freeCodeCam/array.js
+++ freeCodeCam/array.js
@@ -10,11 +10,13 @@
 
  3) Access array data with index
 
- 4) Manipulate Arrays with push()
+ 4) Manipulate Arrays with .push(), .pop(),  .shift(),  .unshift()
 
- 5)
+ 5) Shopping List  Nested  array/ multidimentional Array
 
- 6)
+
+
+ 6) Manipulate Arrays with splice()  by Naveen
 
  7)
 
@@ -112,6 +114,8 @@ console.log(anArray, "\n\n");
 
 
 
+
+
 // Manipulate Arrays with pop()
 
 
@@ -145,7 +149,7 @@ console.log(onArr);
 var removedArr = onArr.shift()
 console.log(onArr);
 
-console.log(removedArr);
+console.log(removedArr, "\n");
 
 
 
@@ -153,8 +157,75 @@ console.log(removedArr);
 
 
 
+// Manipulate Arrays with shift()
 
 
+var anArr = ["James", "Jony", "Paul"]
+console.log(anArr)
+
+var addedArr = anArr.unshift("Harry")
+console.log(anArr)
+
+var anArr = ["Raymon", 43, "Hasib"]
+console.log(anArr)
+
+var addArr = anArr.unshift(56)
+console.log(anArr, "\n\n")
+
+
+
+
+
+// Manipulate Arrays with splice()
+
+
+
+
+
+//  Shopping List  Nested  array/ multidimentional Array
+
+
+
+
+var aList = [["cereal",3], ["milk", 2], ["juice", 2], ["eggs", 12], 53, "bags" ];
+
+console.log("Main List:- ",aList);
+
+var removeItem = aList.pop()
+console.log("poped List",aList);
+
+var shift = aList.shift();
+console.log("shifted List", aList);
+
+
+var unShift = aList.unshift(["Bananas", 8]);
+console.log("unshifted List", aList);
+
+
+var push = aList.push("Mango", "Orange")
+console.log(aList);
+
+console.log("\n######## freeCodeCam######### code is done here\n\n")
+
+
+
+
+
+
+//Manipulate Arrays with splice()    by Naveen
+
+
+var animals = ["dog", "cat", "bird","horse","cow","bird","pig","fish", "Tiger", "Deer"];
+
+console.log(animals)
+
+
+animals.splice(1,1,"Bear");      // here at indx-1 removing 1 element and adding "Bear" at that place.
+console.log(animals);
+
+// here at from index-2 removing 3 element and adding "Lion","Zebra" at that place.
+var addedAnimal = animals.splice(2,3,"Lion","Zebra")
+console.log("\n",animals)
 
 
 
