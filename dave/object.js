d527c378666b24f79e88acd6de4bbd94c4845d87
shefat26 <shefatmoral@gmail.com>
2024-01-09 20:51:29 -0500

    constructor object practice

--- dave/object.js
+++ dave/object.js
@@ -74,7 +74,24 @@ console.log("\n\n")
 
 
 
+
+
+
+
+
+
+
+
+/*
+
+// Example:-2
 // object.create()
+
+=> Using constructor Obj "Object.create()", we can Inherit different 
+object in to constructor object.
+=> basically we are inheriting object property into new obj 
+
+*/
 const vehicle = {
 
     wheels: 4,
@@ -87,19 +104,20 @@ const vehicle = {
 }
 
 
-// we are using generic obj "vehicle" as a constructor for truck obj
-const truck = Object.create(vehicle)
-
-truck.doors = 2;
-console.log(truck);
-
-//Inheriting vehicle obj property value
-console.log(truck.wheels);    
-console.log(truck.engine());
-
-
 
 // we are using generic obj "vehicle" as a constructor for truck obj
+// const truck = Object.create(vehicle)
+
+// truck.doors = 2;
+// console.log(truck);
+
+// //Inheriting vehicle obj property value
+// console.log(truck.wheels);    
+// console.log(truck.engine());
+
+
+
+// we are using generic obj "vehicle" as a constructor for "car" obj
 const car = Object.create(vehicle);
 car.doors = 4;
 car.engine = function(){
@@ -108,8 +126,32 @@ car.engine = function(){
 
 };
 
-
 console.log(car);
 
 console.log(car.engine());
 console.log(car.wheels);
+
+
+
+// we are using generic obj "car" as a constructor for "tesla" obj
+// In other words we are injecting "car" obj into "tesla" obj
+const tesla = Object.create(car)
+
+tesla.mirror = 2;
+tesla.window =  5;
+tesla.exhost = "None"
+console.log(tesla);
+
+console.log(tesla.mirror)
+
+console.log(tesla.wheels)
+
+tesla.engine = function(){
+
+    return "Shhhhhh....";
+
+};
+
+console.log(tesla.engine())
+
+
