2b453f481fed07b87eca821132ba641a9617a3c7
shefat26 <shefatmoral@gmail.com>
2024-01-14 19:00:28 -0500

    array methods practice

--- arrowFunction.js
+++ arrowFunction.js
@@ -70,13 +70,74 @@ let bio = (details)=>{
 
 console.log(bio(person));
 
+console.log("\n#################\n")
 
 
 
 
 
+// Array method()
+
+
+const companies= [
+    {name: "Company One", category: "Finance", start: 1981, end: 2004},
+    {name: "Company Two", category: "Retail", start: 1992, end: 2008},
+    {name: "Company Three", category: "Auto", start: 1999, end: 2007},
+    {name: "Company Four", category: "Retail", start: 1989, end: 2010},
+    {name: "Company Five", category: "Technology", start: 2009, end: 2014},
+    {name: "Company Six", category: "Finance", start: 1987, end: 2010},
+    {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
+    {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
+    {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
+  ];
+  
+  const ages =[33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 
+               25, 64, 32];
+
+
+  
+// calling all companies using for loop
+
+for(let i = 0; i < companies.length; i++){
+
+   // console.log(companies[i]);
+}
+
+console.log("\n###########\n")
 
 
 
+// forEach()
+
+ companies.forEach(function(company){
+
+    console.log(company.category);
+ })
+
+
+//  let name = (param)=> companies.start ;
+//  console.log(name(companies))
+
+
+
+console.log()
+
+// filter()
+
+
+
+let canDrink = [] ;
+
+for(let i = 0; i < ages.length; i++){
+
+    if(ages[i] >= 21){
+
+        canDrink.push(ages[i]);
+
+    }
+
+}
+
+console.log(canDrink)
 
 
