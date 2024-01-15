b4ff323eea8049c0e7942894bba14a48e6a9db8d
shefat26 <shefatmoral@gmail.com>
2024-01-15 18:13:15 -0500

    object  practice

--- Naveen/object.js
+++ Naveen/object.js
@@ -198,6 +198,45 @@ console.log(dept1.deptName, dept1.hod);
 
 
 
+// 1. Object with nested obj, Array and different data types and also with methods
+
+let objt = {
+
+    alive: true,
+    answer: 42,
+    hobbies: ["Eat", "Sleep", "Code" , "practice", "A", "B", 1, 2, 3, true, false],
+    degree: [9,6,7, 12, 8,3,99,44,32],
+    beverage: {
+        morning: "Coffee",
+        afternoon: "Iced Tea"
+
+    },
+
+    //adding anonymous function inside the obj
+    action: function(){
+
+        return "Hellow World"
+    },
+
+    anotheract: function(){
+
+        return `This is the time for ${this.beverage.morning}`
+        
+    },
+
+    c: "abcd"
+ }
+
+  console.log(objt.alive)
+  console.log(objt)
+
+ // accessing objects array index value
+ //console.log(objt.hobbies[1])
+
+ console.log(objt.degree[objt.degree.length-1]);
+ console.log(objt.degree[objt.degree.length-2]);
+ console.log(objt.hobbies[objt.hobbies.length-1]);
+
 
 
 
