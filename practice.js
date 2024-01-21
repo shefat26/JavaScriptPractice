4266355e1690063429cd2b8cc44d5588c271c83f
shefat26 <shefatmoral@gmail.com>
2024-01-21 18:15:30 -0500

    practice code2

--- practice.js
+++ practice.js
@@ -93,6 +93,115 @@ function new1(param) {
   
   
 
+  ///
+
+  const sayHello= ()=>  "hellow world"
+
+console.log(sayHello());
+
+
+
+// single parameter
+
+const squar = (num) => num*num ;
+
+console.log(squar(5));
+
+
+
+// add 
+const  add = (a, b) =>{
+
+    const sum = a + b;
+
+    return sum ;
+
+}
+
+console.log(add(5,9));
+
+
+
+
+// multiply
+
+let multiply = (a,b)=>{
+
+    return a*b;
+
+}
+
+console.log(multiply(6,7))
+
+
+
+//
+
+let arr = [10,12,15,13,14,11, 17, 19]
+
+let a = []
+let compare = (num)=>{
+
+   
+
+    for (let i = 0; i< arr.length; i++){
+
+        if(num[i] >= 13){
+
+            return a.push.num[i];
+        }
+    }
+}
+
+console.log(compare(arr))
+
+
+
+
+
+// cons
+
+const greater = (a,b) => {
+
+    if( a > b){
+
+        return " A is greater"
+
+    }
+    else{
+
+        return " B is greater"
+    }
+}
+
+console.log(greater(7,5));
+
+
+
+
+// Arrow function with obj
+
+
+let person = {
+
+    firstName: "John",
+    lastName: "Adams",
+    age: 27
+
+}
+
+let bio = (details)=>{
+
+    return `${details.firstName} ${details.lastName} is ${details.age} years`
+
+}
+
+console.log(bio(person));
+
+
+
+
+
 
 
 
