bc1c81123408cd38dcf42c401c79486139a62ec9
shefat26 <shefatmoral@gmail.com>
2024-01-24 19:49:16 -0500

    destructring and Ternary practice

---
+++ Naveen/classConcept.js
@@ -0,0 +1,44 @@
+
+
+
+
+// Object with ternary operator
+
+let users1 = [
+    { firstName: "Susan", lastName: "Steward", age: 17, hobby: "Singing", salary: 30000 },
+    { firstName: "Daniel", lastName: "Longbottom", age: 18, hobby: "Football", salary: 70000 },
+    { firstName: "Jacob", lastName: "Black", age: 19, hobby: "Singing", salary: 50000 },
+  ];
+  
+  let mapped1 = users1.map((u) => {
+  
+  
+    return {
+      ...u,
+      
+      fullName : u.firstName + "" + u.lastName,
+      adult : u.age >= 18 ? true : false, 
+      minor : u.age <18 ? true : false,
+      salary: u.salary <= 30000 ? "lowerClass" : "middleClass"
+      
+    };
+  });
+  console.log(mapped1);
+  
+  
+  // Destructring object
+  
+  let obj8 = {
+    firstName: "Susan",
+    lastName: "Steward",
+    age: 17,
+    hobby: "Singing",
+  };
+  
+  let { firstName, lastName, age, hobby } = obj8;
+  
+  console.log(firstName);
+
+  
+
+  
