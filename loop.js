5b6e88995f0883db832e36211a7851594685afee
shefat26 <shefatmoral@gmail.com>
2024-02-05 20:23:53 -0500

    Loop practice

---
+++ loop.js
@@ -0,0 +1,121 @@
+
+
+var flag = true
+
+if (!flag){                        //(!flag) means not true here, we just used quickest way to write false
+
+    console.log("this is valid")
+
+}
+else {
+
+
+    console.log("the second one is true")
+
+}
+
+
+console.log("")
+
+var a = 9
+var b = 6
+var c = 3
+
+if (b>=a){
+
+    console.log("True")
+
+}
+else if (c>=b){
+
+    console.log("Invalid")
+
+}
+else{
+
+    console.log("None of them are correct")
+}
+
+
+console.log("###################################################")
+
+
+
+
+
+// Now we will work withe the While Loops
+
+// If the condition is True while loop will run till the false condition.
+// so the while loop will stop only when the condition is false.
+
+
+let i = 0
+
+while(i<10){
+
+    i++
+    console.log(i)
+}
+
+
+
+console.log("")
+
+var bar = false
+
+while(bar) {
+
+    console.log(bar)
+
+    bar = true
+}
+
+
+
+/////
+
+
+// Now we will will work with do while(){ } Loop
+
+// in do while() { } Loop , before the condition we will have statement ad it will run befor the condition
+// if the condition of the Loop is True/False  the statement will run eventually an then it will go to the conditon
+// if you have any requirements of executuion before loops, then go withe  " do while loop"
+
+
+let d = 0
+
+while(d>10){
+
+ console.log("d is greater than ten")
+
+}
+
+// do{
+
+//   console.log("d is less than Ten")
+//  i++
+// }
+// while(d<10);
+
+
+//     console.log("Ten is greater than d")
+
+
+// console.log("end")
+
+
+
+let required = true
+
+while(required){
+
+    console.log(required)
+
+    required = false
+}
+
+
+
+console.log("#######################")
+
+
