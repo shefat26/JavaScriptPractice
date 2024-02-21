a8f3539b4f1b223473f9c68294c0effb565239b3
shefat26 <shefatmoral@gmail.com>
2024-02-21 18:42:14 -0500

    Array method practice

---
+++ practiceArr.js
@@ -0,0 +1,87 @@
+
+
+// 4) Manipulate Arrays with push()
+
+
+
+var anArray = ["John", "jay", "cat"];
+console.log(anArray);
+
+
+anArray.push("Ron", "Joy");
+console.log(anArray);
+
+
+var anArray = [["James", 23], ["Dog", 3]];
+console.log(anArray)
+
+anArray.push(["bird",1])
+console.log(anArray, "\n\n");
+
+
+
+
+
+
+
+// Manipulate Arrays with pop()
+
+
+var newArr = [1,2,3,4]
+console.log(newArr)
+
+var removedArr = newArr.pop()
+
+console.log(newArr);
+console.log(removedArr,"\n");
+
+
+
+
+
+// Manipulate Arrays with shift()
+
+var otherArr = ["stimpson", "J", ["cat"]];
+console.log(otherArr);
+
+var removedArr = otherArr.shift();
+console.log(otherArr);
+
+console.log(removedArr, "\n");
+
+
+
+var onArr = [["John",23], ["dog",3], ["Bird", 1]];
+console.log(onArr);
+
+var removedArr = onArr.shift()
+console.log(onArr);
+
+console.log(removedArr, "\n");
+
+
+
+
+
+
+
+// Manipulate Arrays with shift()
+
+
+var anArr = ["James", "Jony", "Paul"]
+console.log(anArr)
+
+var addedArr = anArr.unshift("Harry")
+console.log(anArr)
+
+var anArr = ["Raymon", 43, "Hasib"]
+console.log(anArr)
+
+var addArr = anArr.unshift(56)
+console.log(anArr, "\n\n")
+
+
+
+
+
+
